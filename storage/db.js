ee2dce163220313b682bd9daaea9ab7cdbc6a2e2
Kike <kike.mendoza9978@gmail.com>
2024-08-13 14:17:11 -0600

    Create all functions for data manipulation with db

--- storage/db.js
+++ storage/db.js
@@ -8,57 +8,106 @@ async function getAlbums() {
 }
 
 async function getAlbumById(albumId) {
-	return this.storage.albums[albumId];
+	console.log("Loading...");
+	const { rows } = await pool.query("SELECT * FROM albums WHERE id = $1", [
+		albumId,
+	]);
+	console.log("Done");
+	return rows[0];
 }
 
 async function getArtistById(artistId) {
-	return this.storage.artists[artistId];
+	console.log("Loading...");
+	const { rows } = await pool.query("SELECT * FROM artists WHERE id = $1", [
+		artistId,
+	]);
+	console.log("Done");
+	return rows[0];
 }
 
 async function getArtists() {
-	return Object.values(this.storage.artists);
+	console.log("Loading...");
+	const { rows } = await pool.query("SELECT * FROM artists");
+	console.log("Done");
+	return rows;
 }
 
 async function getCompanies() {
-	return Object.values(this.storage.companies);
+	console.log("Loading...");
+	const { rows } = await pool.query("SELECT * FROM companies");
+	console.log("Done");
+	return rows;
 }
 
 async function getCompanyById(companyId) {
-	return this.storage.companies[companyId];
+	console.log("Loading...");
+	const { rows } = await pool.query("SELECT * FROM artists WHERE id = $1", [
+		companyId,
+	]);
+	console.log("Done");
+	return rows[0];
 }
 
 async function getAlbumsByArtistId(artistId) {
-	return Object.values(this.storage.albums).filter(
-		(album) => album.artistId === artistId,
+	console.log("Loading...");
+	const { rows } = await pool.query(
+		"SELECT * FROM albums JOIN artists ON albums.artistIs = artist.id WHERE artist.id = $1",
+		[artistId],
 	);
+	console.log("Done");
+	return rows;
 }
 
 async function addAlbum(albumData) {
-	this.storage.albums[this.id] = new Album(
-		albumData.title,
-		albumData.artistId,
-		albumData.member,
-		albumData.price,
-		albumData.imgUrl,
-		albumData.reviewScore,
-		albumData.discountPrice,
-		this.id,
-		albumData.releaseDate,
+	console.log("Loading...");
+	await pool.query(
+		"INSERT INTO albums (title, artistId, member,price, imgUrl, reviewScore, discountPrice, releaseDate) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)",
+		[
+			albumData.title,
+			albumData.artistId,
+			albumData.member,
+			albumData.price,
+			albumData.imgUrl,
+			albumData.reviewScore,
+			albumData.discountPrice,
+			albumData.releaseDate,
+		],
 	);
-	this.id++;
+	console.log("Done");
 }
 
 async function addArtist(artistData) {
-	this.storage.artists[this.id] = new Artist(
-		this.id,
-		artistData.name,
-		artistData.debutDate,
-		artistData.imgUrl,
-		artistData.companyId,
+	console.log("Loading...");
+	await pool.query(
+		"INSERT INTO artists (name, debutDate, imgUrl,companyId) VALUES ($1, $2, $3, $4)",
+		[
+			artistData.name,
+			artistData.debutDate,
+			artistData.imgUrl,
+			artistData.companyId,
+		],
 	);
-	this.id++;
+	console.log("Done");
 }
 async function addCompany(companyData) {
-	this.storage.companies[this.id] = new Company(this.id, companyData.name);
-	this.id++;
+	console.log("Loading...");
+	await pool.query("INSERT INTO companies (name, ) VALUES ($1)", [
+		companyData.name,
+	]);
+	console.log("Done");
 }
+
+const db = {
+	getAlbums,
+	getArtists,
+	getCompanies,
+	getAlbumById,
+	getAlbumsByArtistId,
+	getArtistById,
+	addAlbum,
+	addArtist,
+	addCompany,
+	getCompanyById,
+};
+
+export default db;
--- storage/populate.js
+++ storage/populate.js
@@ -1,5 +1,13 @@
 import { Client } from "pg";
-const SQL = `
+
+const client = new Client({
+	host: process.env.LOCAL_DB_HOST,
+	user: process.env.LOCAL_DB_USER,
+	database: process.env.LOCAL_DB_DATABASE,
+	password: process.env.LOCAL_DB_PASSWORD,
+	port: process.env.LOCAL_DB_PORT,
+});
+const SQLAlbums = `
 CREATE TABLE IF NOT EXISTS albums (
 id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 title TEXT NOT NULL,
@@ -16,8 +24,22 @@ VALUES
 ('X X', 1, 29.9, NULL, 'https://i.scdn.co/image/ab67616d0000b27316caa9e546f536939373fb26', 5, $1);
 
 `;
+const SQLArtists = `
+CREATE TABLE IF NOT EXISTS artists (
+id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+name TEXT NOT NULL,
+imgUrl TEXT,
+debutDate DATE,
+companyId INTEGER);
 
-async function populate() {
+INSERT INTO artists (name, imgUrl, debutDate)
+VALUES
+('LOONA', 'https://akamai.sscdn.co/uploadfile/letras/fotos/1/4/0/b/140bfcca76325e621e62fa6156d96796.jpg', $1),
+('WooAh!', 'https://thebiaslist.com/wp-content/uploads/2024/04/wooah-blush.jpg', $1);
+
+`;
+
+async function populateAlbums() {
 	console.log("Sending...");
 	const client = new Client({
 		host: process.env.LOCAL_DB_HOST,
@@ -27,31 +49,38 @@ async function populate() {
 		port: process.env.LOCAL_DB_PORT,
 	});
 	await client.connect();
-	await client.query(SQL, [new Date()]);
+	await client.query(SQLAlbums, [new Date()]);
 	await client.end();
 	console.log("Done");
 }
-
-const client = new Client({
-	host: process.env.LOCAL_DB_HOST,
-	user: process.env.LOCAL_DB_USER,
-	database: process.env.LOCAL_DB_DATABASE,
-	password: process.env.LOCAL_DB_PASSWORD,
-	port: process.env.LOCAL_DB_PORT,
-});
+async function populateArtists() {
+	console.log("Sending...");
+	await client.connect();
+	await client.query(SQLArtists, [new Date()]);
+	await client.end();
+	console.log("Done");
+}
 
 async function queryTest() {
 	console.log("Reading...");
 	await client.connect();
 	const { rows } = await client.query("SELECT * FROM albums");
-	await client.end();
 	console.log("Done");
 	return rows;
 }
 
 async function printQuery() {
-	const rows = await queryTest();
-	console.log(rows);
+	console.log("Loading...");
+	await client.connect();
+
+	const { rows } = await client.query(
+		"SELECT * FROM albums WHERE id = $1",
+		[1],
+	);
+	await client.end();
+	console.log("Done");
+	console.log(rows[0]);
+	return rows;
 }
 
-printQuery();
+populateArtists();
