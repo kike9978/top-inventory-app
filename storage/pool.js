3f26879770f624d8a5f01cd26de7055f7ede052f
Kike <kike.mendoza9978@gmail.com>
2024-08-14 07:06:10 -0600

    Configure env

--- storage/pool.js
+++ storage/pool.js
@@ -3,15 +3,16 @@ dotenv.config();
 import pkg from "pg";
 const { Pool } = pkg;
 
+const env = process.env.ENV || "development";
 const dbSrc = {
-	devDb: {
+	development: {
 		host: process.env.LOCAL_DB_HOST,
 		user: process.env.LOCAL_DB_USER,
 		database: process.env.LOCAL_DB_DATABASE,
 		password: process.env.LOCAL_DB_PASSWORD,
 		port: process.env.LOCAL_DB_PORT,
 	},
-	prodDb: {
+	production: {
 		host: process.env.REMOTE_DB_HOST,
 		user: process.env.REMOTE_DB_USER,
 		database: process.env.REMOTE_DB_DATABASE,
@@ -20,13 +21,12 @@ const dbSrc = {
 	},
 };
 
-console.log(process.argv[2]);
 const pool = new Pool({
-	host: dbSrc[process.argv[2]].host,
-	user: dbSrc[process.argv[2]].user,
-	database: dbSrc[process.argv[2]].database,
-	password: dbSrc[process.argv[2]].password,
-	port: dbSrc[process.argv[2]].port,
+	host: dbSrc[env].host,
+	user: dbSrc[env].user,
+	database: dbSrc[env].database,
+	password: dbSrc[env].password,
+	port: dbSrc[env].port,
 });
 
 export default pool;
